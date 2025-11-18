0b8268b90a768b3a06daa8a9564c2ddf997cefc3
João Victor Astle <jvliv123@gmail.com>
2025-11-18 17:18:20 -0300

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,1 +1,17 @@
+# projeto: Jogo da Velha em MIPS
+# autor: João
+
+.data
+msgStart: .asciiz "Jogo da Velha em MIPS iniciado...\n"
+
+.text
+main:
+    # printar mensagem inicial
+    li $v0, 4
+    la $a0, msgStart
+    syscall
+
+    # encerrar programa
+    li $v0, 10
+    syscall
 
